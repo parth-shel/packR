2bf7fe3b2db9113ee2beafd6edb8bd204ff268f3
parth-shel <ps@parthshel.com>
2018-05-12 23:13:42 +0530

    messing around with inflate and deflate crates

--- src/pack/mod.rs
+++ src/pack/mod.rs
@@ -1,3 +1,18 @@
+extern crate deflate;
+extern crate inflate;
+
+use self::deflate::deflate_bytes;
+use self::inflate::inflate_bytes;
+use std::str::from_utf8;
+
 pub fn pack(file_path: &str) {
-	println!("packking: {}", file_path);
+	println!("packing: {}", file_path);
+
+	let data = "Hello World!";
+	let compressed = deflate_bytes(data.as_bytes());
+	println!("{:?}", compressed);
+
+	let encoded = compressed;
+	let decoded = inflate_bytes(&encoded).unwrap();
+	println!("{}", from_utf8(&decoded).unwrap()); // prints "Hello, world"
 }
