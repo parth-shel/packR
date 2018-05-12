065fbc687c5d4b8f9411bcfd3eb91e2030c70fd0
parth-shel <ps@parthshel.com>
2018-05-13 00:58:28 +0530

    compression works!

--- src/pack/mod.rs
+++ src/pack/mod.rs
@@ -5,14 +5,84 @@ use self::deflate::deflate_bytes;
 use self::inflate::inflate_bytes;
 use std::str::from_utf8;
 
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::metadata;
+use std::fs::read_dir;
+
 pub fn pack(file_path: &str) {
 	println!("packing: {}", file_path);
 
+	let md = metadata(file_path).unwrap();
+
+	if md.is_file() {
+		handle_file(file_path);
+	}
+
+	if md.is_dir() {
+		handle_dir(file_path);
+	}
+
+	/*
+	// compress
 	let data = "Hello World!";
 	let compressed = deflate_bytes(data.as_bytes());
 	println!("{:?}", compressed);
 
+	// decompress
 	let encoded = compressed;
 	let decoded = inflate_bytes(&encoded).unwrap();
-	println!("{}", from_utf8(&decoded).unwrap()); // prints "Hello, world"
+	println!("{}", from_utf8(&decoded).unwrap());
+	 */
+}
+
+fn handle_file(file_path: &str) {
+	// create path to desired file
+	let path = Path::new(file_path);
+	let display = path.display();
+
+	// open path in read-only mode
+	let mut file = match File::open(&path) {
+		Err(why) => panic!("couldn't open {}: {}", display, why.description()),
+			Ok(file) => file,
+	};
+
+	// read the contents of the file into a string
+	let mut data = String::new();
+	match file.read_to_string(&mut data) {
+		Err(why) => panic!("couldn't read {}: {}", display, why.description()),
+			Ok(_) => /*print!("{} contains:\n{}", display, data)*/(),
+	}
+
+	// compress the data
+	let compressed = deflate_bytes(data.as_bytes());
+
+	let mut out_file = String::new();
+	out_file.push_str(file_path);
+	out_file.push_str(".pack");
+
+	let out_path = Path::new(&out_file);
+	let out_display = out_path.display();
+
+	// open a file in write-only mode
+	let mut file = match File::create(&out_path) {
+		Err(why) => panic!("couldn't create {}: {}", out_display, why.description()),
+			Ok(file) => file,
+	};
+
+	// write the compressed data to the file
+	match file.write_all(&compressed) {
+		Err(why) => {panic!("couldn't write to {}: {}", out_display, why.description())},
+			Ok(_) => println!("successfully wrote to {}", out_display),
+	}
+}
+
+fn handle_dir(dir_path: &str) {
+	let paths = read_dir(dir_path).unwrap();
+
+	for path in paths {
+		println!("dir entry: {}", path.unwrap().path().display());	
+	}
 }
